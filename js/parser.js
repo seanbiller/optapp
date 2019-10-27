1426d6d2fe485f2eb6524e4d11b119d993185683
Sean Biller <seanmbiller@gmail.com>
2019-10-27 16:45:21 -0400

    parser change

--- js/parser.js
+++ js/parser.js
@@ -1,3 +1,5 @@
+const indexjs = require("index.js")
+
 const selectedFile = document.getElementById('input').files[0];
         NodeStl(selectedFile + '/myCool.stl', {density: 1.04});
                 console.log(stl.volume + 'cm^3');     // 21cm^3

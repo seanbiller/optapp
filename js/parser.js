a3348ed8634da08686805a3229f7012422a72016
Connor Michael Gerrits <connormgerrits@connors-air.worcester.local>
2019-11-15 20:26:39 -0500

    deleted a few console.log() lines

--- js/parser.js
+++ js/parser.js
@@ -1,12 +1,10 @@
 
 const input = document.querySelector('input[type="file"]');
 input.addEventListener('change', function (e) {
-    //console.log(input.files)
     const reader = new FileReader()
     reader.onload = function () {
         var fileContents = reader.result
         document.getElementById("STLdataview").value = fileContents;
-        //console.log(reader.result)
     }
     reader.readAsText(input.files[0], "UTF-8")
 }, false)

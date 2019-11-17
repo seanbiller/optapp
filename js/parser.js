76f802867f49793ce2dc5787eb29e93e87f1ac0e
Connor Michael Gerrits <connormgerrits@connors-air.worcester.local>
2019-11-17 15:46:55 -0500

    deleted commented-out 'node-stl' console.log statement attempts

--- js/parser.js
+++ js/parser.js
@@ -1,11 +1,10 @@
+const input = document.querySelector('input[type="file"]')
 
-const input = document.querySelector('input[type="file"]');
 input.addEventListener('change', function (e) {
     const reader = new FileReader()
     reader.onload = function () {
         var fileContents = reader.result
-        document.getElementById("STLdataview").value = fileContents;
+        document.getElementById("STLdataview").value = fileContents
     }
     reader.readAsText(input.files[0], "UTF-8")
 }, false)
-

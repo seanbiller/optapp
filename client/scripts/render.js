26279e7ec19a369701d57c6a8d6c3b183ea67f6f
Sean Biller <seanmbiller@gmail.com>
2019-12-09 22:14:38 -0500

    Fish Fully Functional

--- client/scripts/render.js
+++ client/scripts/render.js
@@ -1,19 +1,15 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500);
+var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
 document.body.appendChild(renderer.domElement);
 
 
-// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// var cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
 var loader = new THREE.STLLoader();
 	// console.log({__dirname})
 loader.load('./client/scripts/fish.stl', function (geometry) {
+
 		console.log(geometry)
 	scene.add(new THREE.Mesh(geometry));
 });
@@ -26,3 +22,4 @@ var animate = function () {
 };
 
 animate();
+

c6370cef10c60a1624668fac33e52f41c322d4f2
Connor M. Gerrits <cgerrits555@gmail.com>
2019-12-13 22:15:45 -0500

    Fixed issues with model's mesh when rendered and added a reflective material layer in stlViewer

--- client/scripts/stlViewerHTML5.js
+++ client/scripts/stlViewerHTML5.js
@@ -1,8 +1,7 @@
 function STLViewerHTML5(geometry, elementID) {
     var elem = document.getElementById(elementID)
 
-    var camera = new THREE.PerspectiveCamera(50,
-      elem.clientWidth / elem.clientHeight, 1, 1000);
+    var camera = new THREE.PerspectiveCamera(50, elem.clientWidth / elem.clientHeight, 1, 1000);
 
     var renderer = new THREE.WebGLRenderer({
       antialias: true,
@@ -27,33 +26,49 @@ function STLViewerHTML5(geometry, elementID) {
 
     var scene = new THREE.Scene();
     scene.add(new THREE.HemisphereLight(0xffffff, 1.5));
+    
+    camera.position.set(0,0,100);
+		scene.add(camera);
 
-      var material = new THREE.MeshPhongMaterial({
-        color: 0xff5533,
-        specular: 100,
-        shininess: 100
-      });
-      var mesh = new THREE.Mesh(geometry, material);
-      scene.add(mesh);
+    geometry.computeVertexNormals()
+    geometry.normalizeNormals()
 
-      var middle = new THREE.Vector3();
-      geometry.computeBoundingBox();
-      geometry.boundingBox.getCenter(middle);
-      mesh.position.x = -1 * middle.x;
-      mesh.position.y = -1 * middle.y;
-      mesh.position.z = -1 * middle.z;
+    if (geometry.hasColors) {
+      var material = new THREE.MeshPhongMaterial({ opacity: geometry.alpha, vertexColors: THREE.VertexColors });
+    }
+    else {
+      //var material = new THREE.MeshStandardMaterial();
+      var material=new THREE.MeshLambertMaterial({color:0x909090, overdraw: 1, wireframe: false, shading:THREE.FlatShading, vertexColors: THREE.FaceColors});
+    }
+    var mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
-      var largestDimension = Math.max(geometry.boundingBox.max.x,
-        geometry.boundingBox.max.y,
-        geometry.boundingBox.max.z)
-      camera.position.z = largestDimension * 1.5;
-      camera.position.y = largestDimension * 1.5;
-      
+    var middle = new THREE.Vector3();
+    geometry.computeBoundingBox();
+    geometry.boundingBox.getCenter(middle);
+    mesh.position.x = -1 * middle.x;
+    mesh.position.y = -1 * middle.y;
+    mesh.position.z = -1 * middle.z;
 
-      var animate = function () {
-        requestAnimationFrame(animate);
-        controls.update();
-        renderer.render(scene, camera);
-      };
-      animate();
+    var largestDimension = Math.max(geometry.boundingBox.max.x,
+      geometry.boundingBox.max.y,
+      geometry.boundingBox.max.z)
+    camera.position.z = largestDimension * 1.5;
+    camera.position.y = largestDimension * 1.5;
+
+    // ambientLight = new THREE.AmbientLight(0x202020);
+    // camera.add(ambientLight);
+  
+    pointLight = new THREE.PointLight(0xffffff, 0.3);
+    pointLight.position.x = 0;
+    pointLight.position.y = -25;
+    pointLight.position.z = 10;
+    camera.add(pointLight);	
+
+    var animate = function () {
+      requestAnimationFrame(animate);
+      controls.update();
+      renderer.render(scene, camera);
+    };
+    animate();
   }

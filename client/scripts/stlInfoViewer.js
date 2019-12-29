413d438ae98f34a7065186993e3b77d65f328e9d
Connor M. Gerrits <cgerrits555@gmail.com>
2019-12-28 19:43:53 -0500

    added in the beginnings of an info table displaying data and display options for the rendered stl model, which appears adjacent to the html element in which the model itself is rendered

--- client/scripts/stlInfoViewer.js
+++ client/scripts/stlInfoViewer.js
@@ -4,6 +4,13 @@
  * which will then be added to index.html and displayed adjacent to the rendered 3D model.
  */
 
-function stlInfoViewer(geometry) {
+function stlInfoViewer(geometry, elementID) {
+    var elem = document.getElementById(elementID);
 
+    geometry.computeBoundingBox();
+    var bbox = geometry.boundingBox;
+    let middle = new THREE.Vector3(geometry.center());
+
+    bbox.getSize(middle);
+    console.log("Dimensions of Model's Bounding-Box: " + JSON.stringify(middle));
 }
